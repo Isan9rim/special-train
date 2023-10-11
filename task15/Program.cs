ae793b5d8c19681091217d02f48a77ef0c1d2d5d
Andrey <Ligenman@example.com>
2023-10-11 16:30:42 +0300

    домашка2

--- task15/Program.cs
+++ task15/Program.cs
@@ -4,22 +4,19 @@
 1 -> нет
 */
 
-System.Console.WriteLine("Введи число: ");
-int weekDay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер дня недели от 1 до 7: ");
 
-int day1 = 1;
-int day2 = 2;
-int day3 = 3;
-int day4 = 4;
-int day5 = 5;
-int day6 = 6;
-int day7 = 7;
+int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-if (weekDay == day1) Console.WriteLine("понедельник рабочий день");
-else if (weekDay == day2) Console.WriteLine("вторник рабочий день");
-else if (weekDay == day3) Console.WriteLine("среда рабочий день");
-else if (weekDay == day4) Console.WriteLine("четверг рабочий день");
-else if (weekDay == day5) Console.WriteLine("пятница рабочий день");
-else if (weekDay == day6) Console.WriteLine("суббота выходной ");
-else if (weekDay == day7) Console.WriteLine("воскресенье выходной ");
-else Console.WriteLine("Введено неверное число");
+if (dayNumber <=5 && dayNumber > 0)
+{
+    System.Console.WriteLine("Это рабочий день!");
+}
+if (dayNumber == 6 || dayNumber ==7)
+{
+    System.Console.WriteLine("Ура выходной!");
+}
+else if (dayNumber < 1 || dayNumber > 7)
+{
+    System.Console.WriteLine("Такого дня недели не сущесьвует! ты че...");
+}
